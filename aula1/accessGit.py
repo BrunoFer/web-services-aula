21172eef8bbd95566ba4ff62504107b8ecbcbb9a
BrunoFer <bferreira129@gmail.com>
2012-10-10 17:55:35 -0300

    aula 1

---
+++ aula1/accessGit.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+import sys
+
+url = 'https://api.github.com/users/{}'.format(sys.argv[1])
+
+import json
+import requests
+import Image
+import urllib
+
+r = requests.get(url)
+data = json.loads(r.read())
+
+if 'name' in data:
+    print 'Nome: ', data['name']
+if 'url' in data:
+    print 'URL: ', data['url']
+if 'location' in data:
+    print 'Localização: ', data['location']
+if 'email' in data:
+    print 'Email: ', data['email']
+if 'login' in data:
+    print 'Login: ', data['login']
+if 'blog' in data:
+    print 'Blog: ', data['blog']
+if 'avatar_url' in data:
+    print 'Avatar: ', data['avatar_url']
+
+import PIL
+from PIL import Image
+
+basewidth = 300
+urllib.urlretrieve(data['avatar_url'],'imagem.png')
+img = Image.open('imagem.png')
+wpercent = (basewidth / float(img.size[0]))
+hsize = int((float(img.size[1]) * float(wpercent)))
+img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+img.save('resized_image.png')
+Image.open('resized_image.png').show()
